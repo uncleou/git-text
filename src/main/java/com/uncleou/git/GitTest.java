ae3892520d8535bddaadea8741960179058d2646
yihaoou <1260376331@qq.com>
2022-09-23 17:59:43 +0800

    second commit

--- src/main/java/com/uncleou/git/GitTest.java
+++ src/main/java/com/uncleou/git/GitTest.java
@@ -3,5 +3,6 @@ package com.uncleou.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git111");
+        System.out.println("hello git222");
     }
 }
